ad5b2d1480289791c47920a402c2e6467b440dc3
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-20 14:19:14 +0200

    ADD 6 file

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%zu\n", sizeof(char));
+	printf("%zu\n", sizeof(int));
+	printf("%zu\n", sizeof(long int));
+	printf("%zu\n", sizeof(long long int));
+	printf("%zu\n", sizeof(float));
+	return (0);
+}
