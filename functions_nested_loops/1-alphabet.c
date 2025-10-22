a124676405a95fff398711fd005a9faeb24c3e02
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-22 09:28:04 +0200

    ADD 1 file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char l = "a";
+
+	while (n <= "z")
+	{
+		_putchar(l);
+		l++;
+	}
+
+	_putchar('\n');
+ 	return (0);
+}
