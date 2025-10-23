c53bdfc480c14037d8c9b8b886878c45d1c8f581
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-23 11:09:52 +0200

    ADD 10 file

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Fonction qui additionne deux entiers
+ * @n: premier entier à additionner
+ * @n2: second entier à additionner
+ *
+ * Return: la somme de n et n2
+ */
+
+int add(int n, int n2)
+{
+	return (n + n2);
+}
