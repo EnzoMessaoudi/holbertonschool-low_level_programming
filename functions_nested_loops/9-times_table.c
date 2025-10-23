2279ba3913e53ca81405c21a1f4a812ca82308ff
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-23 10:50:48 +0200

    Change 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
-* main - Entry point
+* times_table - function that shows the table multiplication
 *
-* Return: Always 0 (Success)
+*
 */
 
 void times_table(void)
 {
- int nombre, multi, prod;
+	int nombre, multi, prod;
 
-    for (nombre = 0; nombre <= 9; nombre++)
-    {
-        for (multi = 0; multi <= 9; multi++)
-        {
-            prod = nombre * multi;
-            if (multi != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (prod < 10)
-                    _putchar(' ');
-            }
-            if (prod >= 10)
-            {
-                _putchar((prod / 10) + '0');
-                _putchar((prod % 10) + '0');
-            }
-            else
-                _putchar(prod + '0');
-        }
-        _putchar('\n');
-    }
+	for (nombre = 0; nombre <= 9; nombre++)
+	{
+		for (multi = 0; multi <= 9; multi++)
+		{
+			prod = nombre * multi;
+			if (multi != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
+				_putchar(' ');
+			}
+			if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			else
+				_putchar(prod + '0');
+		}
+	_putchar('\n');
+	}
 }
 
