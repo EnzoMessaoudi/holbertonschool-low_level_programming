ed2badc4b6ac2ea220845f8c4b5df6e852843b20
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-27 10:24:51 +0100

    ADD 2 file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - Function that multiple characters
+* @a: character tested
+* @b: character tested
+* Return: Return the value of mul
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
