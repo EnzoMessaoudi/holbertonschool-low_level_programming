d29e977617f63d7bd98a39d61d7602e711ae2c14
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-27 10:32:25 +0100

    ADD 3 file

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_numbers - Function that prints the number from 0 to 9
+*
+*
+*/
+
+void print_numbers(void)
+{
+	for (char n = '0'; n <= '9'; n++)
+		_putchar(n);
+
+	_putchar ('\n');
