d0cf30b739f66d0f54116ac7ae70fa122e3385ae
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-28 11:28:11 +0100

    FIX 9 file

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,15 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
-		else if (n % 5 == 0)
+		else if (n % 5 == 0 && n != 100)
 			printf("Buzz ");
 		else if (n < 100)
 			printf("%d ", n);
 		else
-			printf("%d", n);
+		{
+			printf("Buzz");
+			putchar('\n');
+		}
 	}
-	printf("\n");
 	return (0);
 }
