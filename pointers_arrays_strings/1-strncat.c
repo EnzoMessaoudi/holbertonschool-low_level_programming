c499c5a3e9386c28bf273a84276dd5cbe7010554
EnzoMessaoudi <12168@holbertonstudents.com>
2025-10-31 09:41:12 +0100

    ADD 1 file

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c = 0;
+	int j = 0;
+
+	while (dest[c] != '\0')
+		c++;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[c] = src[j];
+		j++;
+		c++;
+	}
+	dest[c] = '\0';
+
+	return (dest);
+}
