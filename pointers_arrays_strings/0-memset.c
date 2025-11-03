f9cb90c8f9a601f60dd9926bade7daebda75282a
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-03 11:07:58 +0100

    FIX 0 file

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _memset: Function that fills memory with constant byte
+* _memset - Function that add b bytes on the n firsgt columns of s
 * @s: Character source
-* @b: Character we want to change
-* @n: Numbers of character we want to change
-* Return: Return the value of s
+* @b: Character tested
+* @n: Character tested
+* Return: Return the value of _memset
 */
 
 char *_memset(char *s, char b, unsigned int n)
