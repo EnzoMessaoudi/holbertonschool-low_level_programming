a15f1dc791836336a17f84ea287fc71c2357c9c5
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-04 09:44:15 +0100

    FIX 5 file

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,8 +12,8 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	    if (*needle == '\0')
-        return haystack;
+	if (*needle == '\0')
+		return (haystack);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
