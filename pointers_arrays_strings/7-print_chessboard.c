08567ca41db0b726ce1018331cb432a8e96c1455
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-04 10:13:05 +0100

    ADD 7 file

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_chessboard - Function that print a chessboard
+* @a: Character tested
+*
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			if (a[i][j] == ' ')
+			{
+				_putchar('\n');
+				break;
+			}
+			else if (j == 8)
+				_putchar('\n');
+			else
+			{
+			_putchar(a[i][j]);
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+}
