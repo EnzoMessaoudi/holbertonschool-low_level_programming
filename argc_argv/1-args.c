29eb2543af3584d5f7384addefb3c0bf81fbbbad
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-06 11:05:26 +0100

    ADD 1 file

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: Character tested
+* @argv: Character
+* Return: Return 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
