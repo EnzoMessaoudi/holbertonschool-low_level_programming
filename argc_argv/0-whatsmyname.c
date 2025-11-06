762b5bdfc21768a56e73dc4798eafc114f3456be
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-06 10:54:02 +0100

    ADD 0 file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Main function
+* @argc: Character tested
+* @argv: Character tested
+* Return: Return 0
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
