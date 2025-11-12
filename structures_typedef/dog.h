21996fc0bc540b07b76af9a12c3adbef63b2af43
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-12 10:30:29 +0100

    ADD dog.h and README.md

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Struct named dog with 3 elements
+* @name: name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
