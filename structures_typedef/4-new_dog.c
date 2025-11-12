9b80bc4b616b0ce72b64c28a4ce9766ebb485e81
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-12 12:17:35 +0100

    FIX 4 file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dog.h"
-
 /**
 * new_dog - Creates a new dog
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
-* Return: Always return 0
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: pointer to new dog, or NULL if failure
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *new_name, *new_owner;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	d->name = strdup(name);
-	d->owner = strdup(owner);
-
-	if (d->name == NULL || d->owner == NULL)
+	new_name = strdup(name);
+	if (new_name == NULL)
 	{
-		free(d->name);
-		free(d->owner);
 		free(d);
 		return (NULL);
 	}
 
+	new_owner = strdup(owner);
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(d);
+		return (NULL);
+	}
+
+	d->name = new_name;
 	d->age = age;
+	d->owner = new_owner;
 
 	return (d);
 }
