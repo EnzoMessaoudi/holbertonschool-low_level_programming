24a0a6ba8f3e49eed4d50b2add2f07bbefc549ad
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-13 10:07:09 +0100

    FIX 4 file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,15 +7,32 @@
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
- *
  * Return: Pointer to new dog, or NULL on failure
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
+	char *name_copy, *owner_copy;
+	int i, name_len = 0, owner_len;
 
 	if (!d)
 		return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[name_len] != '\0')
+		name_len++;
+	while (owner[owner_len] != '\0')
+		owner_len++;
+
+	name_copy = malloc(name_len + 1);
+	owner_copy = malloc(owner_len + 1);
+
+	for (i = 0; i <= name_len; i++)
+		name_copy[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		owner_copy[i] = owner[i];
+
 
 	d->name = name;
 	d->age = age;
