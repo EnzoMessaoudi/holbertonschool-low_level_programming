fa9c4c9f3f5ebdc4b42025cd0dec4fbea0b5c030
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-17 11:12:34 +0100

    ADD 0 file

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+* print_name - Print a name
+* @name: Name printed
+* @f: Function pointer
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
