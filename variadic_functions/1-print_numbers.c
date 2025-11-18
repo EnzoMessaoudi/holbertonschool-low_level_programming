3edd7aeffc0644fc26ae6bce694ab38cd9513246
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-18 09:51:46 +0100

    ADD 1 FILE

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - Print number passed as parameters
+* @separator: paramter passed by the user that separate tne numbers
+* @n: First number
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n ; i++)
+	{
+		int value = va_arg(args, int);
+
+		if (i == n - 1)
+			printf("%d\n", value);
+		else
+			if (separator)
+				printf("%d%s", value, separator);
+			else
+				printf("%d", value);
+	}
+	va_end(args);
+}
