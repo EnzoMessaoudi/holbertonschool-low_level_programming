9506178d2f99c8b4c4f4f97cf2c0d7d5b38d9de3
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-18 13:31:51 +0100

    FIX 1 file

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,29 +3,30 @@
 #include "variadic_functions.h"
 
 /**
-* print_numbers - Print number passed as parameters
-* @separator: paramter passed by the user that separate tne numbers
-* @n: First number
+* print_numbers - Prints numbers separated by a separator
+* @separator: Given by the user to separate the numbers printed
+* @n: How many of number the user want to print
 */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
+	unsigned int i, value;
 	va_list args;
 
 	va_start(args, n);
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
-		int value = va_arg(args, int);
+		value = va_arg(args, int);
 
 		if (i == n - 1)
 			printf("%d\n", value);
 		else
+		{
 			if (separator == NULL)
 				printf("%d", value);
 			else
 				printf("%d%s", value, separator);
+		}
 	}
-	va_end(args);
 }
