4757c39e66588c0fc06f9b09ca574d0c68d7caa4
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-20 11:06:34 +0100

    ADD 3 file

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_all - Function that print anything passed in parameter
+* @format: Types of the arguments passed
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0;
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+		 format[i] == 'f' || format[i] == 's')
+		{
+			if (format[i] == 'c')
+				printf("%c", va_arg(args, int));
+			else if (format[i] == 'i')
+				printf("%i", va_arg(args, int));
+			else if (format[i] == 'f')
+				printf("%f", va_arg(args, double));
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+			}
+
+			if (format[i + 1])
+				printf(", ");
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	va_end(args);
+}
