b9605c255ef9e99a0eda3ea6513590f4b431bc8c
EnzoMessaoudi <12168@holbertonstudents.com>
2025-11-20 11:13:11 +0100

    FIX 3 file

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -27,7 +27,7 @@ void print_all(const char * const format, ...)
 			if (format[i] == 'c')
 				printf("%c", va_arg(args, int));
 			else if (format[i] == 'i')
-				printf("%i", va_arg(args, int));
+				printf("%d", va_arg(args, int));
 			else if (format[i] == 'f')
 				printf("%f", va_arg(args, double));
 			else if (format[i] == 's')
