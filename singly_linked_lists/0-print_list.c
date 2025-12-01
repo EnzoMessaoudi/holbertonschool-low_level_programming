7e4a99bf798de0a95e2dc249f364c19219fde2ce
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-01 15:17:46 +0100

    Fix lists.h and 0 file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,13 +6,14 @@
 
 /**
 * print_list - Print all elements of list_h
-* @list_h: List given by the user
+* @h: List given by the user
 * Return: Return the number of nodes printed
 */
 
 size_t print_list(const list_t *h)
 {
 	int t = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
