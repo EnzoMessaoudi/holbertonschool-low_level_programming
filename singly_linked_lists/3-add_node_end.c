e0f423d25224be745f5ce1202faf429f59115b9c
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-02 09:06:56 +0100

    ADD 3 file

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* add_node_end - Add a node at the end of **head
+* @head: List given by the user
+* @str: String the user want at the end of the list
+* Return: Return the string
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *last = *head;
+	list_t *newNode = malloc(sizeof(list_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	if (!newNode->str)
+	{
+		free(newNode);
+		return (NULL);
+	}
+
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = newNode;
+
+	return (newNode);
+}
