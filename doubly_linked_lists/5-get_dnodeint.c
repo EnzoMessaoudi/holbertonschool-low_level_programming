6b26e7610dace63fe61c7c0641dd5cbae8b63fd6
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-02 13:31:32 +0100

    ADD 5 file

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* get_dnodeint_at_index - Return the index'th node
+* @head: List given by user
+* @index: Which node the user want to see
+* Return: Return the node the user want
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int c = 0;
+
+	while (c != index)
+	{
+		head = head->next;
+		c++;
+	}
+
+	return (head);
+}
