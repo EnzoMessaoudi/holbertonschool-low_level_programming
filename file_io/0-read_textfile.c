016347a10a10c96085a00f1f2f01a0cda424ee2e
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-03 14:06:00 +0100

    ADD 0 file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+* read_textfile - Read a file and print it in the stdout
+* @filename: Name of the file that we want to read
+* @letters: Chars we want to print
+* Return: Return the number of chars we printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t n;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+	n = read(fd, buf, letters);
+	if (n == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	if (n > 0)
+		n = write (1, buf, n);
+
+	if (n == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (n);
+}
