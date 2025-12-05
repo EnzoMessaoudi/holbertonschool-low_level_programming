fa60adb521cad23f8503c7f5e898f2df34f2b7c1
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-05 17:17:28 +0100

    ADD 4 file

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - Show the value associated at index 'key'
+* @ht: Table we search for the key and value
+* @key: The key of what value we searching for
+* Return: Return the value of the key or NULL if no key is found
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int ind;
+	hash_node_t *current;
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	ind = hash_djb2((unsigned char *) key) % ht->size;
+	current = ht->array[ind];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+
+	return (NULL);
+}
