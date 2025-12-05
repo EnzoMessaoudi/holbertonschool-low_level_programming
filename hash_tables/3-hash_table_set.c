c045e7c82c6efccad0656191fa717e609f58d0a6
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-05 15:33:20 +0100

    Fix 3 file

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,16 +11,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ind =  hash_djb2((unsigned char *)key) % ht->size;
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (0);
+	hash_node_t *new_node;
 
 	if (key == NULL || ht == NULL)
 		return (0);
 
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	
 	new_node->next = NULL;
 
 	if (ht->array[ind] == NULL)
