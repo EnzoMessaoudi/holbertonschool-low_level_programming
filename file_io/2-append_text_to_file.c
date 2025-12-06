274011d165c796f28a5f9e5ffde4f2644c2b8479
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-06 07:46:46 +0100

    Fix 2 file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,16 +12,22 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i = 0, w;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (filename == NULL && fd == 1)
 	{
-		close(fd);
+		close (fd);
+		return (1);
+	}
+
+	if ((text_content == NULL || *text_content == '\0') && fd == -1)
+	{
+		close (fd);
 		return (-1);
 	}
 
