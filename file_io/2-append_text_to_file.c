1c912c56dc321c5dab8a024c564c2e0b4f24405c
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-06 07:51:06 +0100

    Fix 2 file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,20 +15,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (access(filename, W_OK) == -1)
+		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
-	if (filename == NULL && fd == 1)
+	if (text_content == NULL)
 	{
-		close (fd);
+		close(fd);
 		return (1);
-	}
-
-	if ((text_content == NULL || *text_content == '\0') && fd == -1)
-	{
-		close (fd);
-		return (-1);
 	}
 
 	while (text_content[i] != '\0')
